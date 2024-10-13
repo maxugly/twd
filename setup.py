94cdd16a42ffdc139748465f619a91fb2c2c17b2
m4sc0 <contact.masco@gmail.com>
2024-10-13 12:35:25 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -18,5 +18,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
 )
